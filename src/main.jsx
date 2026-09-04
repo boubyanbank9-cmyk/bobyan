11dc4521403f2a32b9d27617bba13fb98547415c
bobyan <boubyanbank9@gmail.com>
2026-09-04 21:34:14 +0300

    Fix router on custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import { SiteSettingsProvider } from './hooks/useSiteSettings'
 import App from './App'
 import './index.css'
 
+const basename = window.location.hostname.endsWith('github.io') ? '/bobyan' : undefined
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <BrowserRouter basename={basename}>
       <SiteSettingsProvider>
         <LanguageProvider>
           <App />
